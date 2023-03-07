decfa2d8cf9ce42859cc5d6ff659feb8a5e5b443
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-03-07 01:01:26 -0800

    Add files via upload
    working with parameters an single expression

--- Main.kt
+++ Main.kt
@@ -1,26 +1,15 @@
 import java.awt.AWTEventMulticaster.add
 import kotlin.math.max
 
-fun main(args:Array<String>) {
-   val person= arrayOf("alit","reza","sara")
-   for (i in person){
-      if (i== "ali"){
-         println(i)
-      }else
-         println("ali is not in the list")
-   }
+
+//work with parameters ,single expression
+fun sayHello(itemTOGreet:String)= println("Hello $itemTOGreet")
 
 
-   //work with map
-   val nameOfMyScore = mutableMapOf(
-      "Name" to "Tahira",
-      "familyName" to "Gholami",
-      "Age" to "thirthy three"
-   )
 
-   println(nameOfMyScore)
-   println(nameOfMyScore.isEmpty())
-   println(nameOfMyScore.size)
+fun main() {
+   sayHello(itemTOGreet = "kotlin")
+   sayHello(itemTOGreet = "world")
 
 }
 
