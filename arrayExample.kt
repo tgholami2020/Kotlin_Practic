d3a05acc380acb4a4a1b5981572180ddc6f5bf66
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-03-08 00:47:28 -0800

    Add files via upload
    Array Example

---
+++ arrayExample.kt
@@ -0,0 +1,12 @@
+
+fun main(){
+    var interestingThings= arrayOf("kotlin","programming","android")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    println(interestingThings.get(2))
+
+    for (i in interestingThings){
+        println(i)
+    }
+
+}
