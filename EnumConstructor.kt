caea5babef1046fea55d7d9569e695d6c12a4aa5
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-03-22 03:04:33 -0700

    Add files via upload

---
+++ EnumConstructor.kt
@@ -0,0 +1,26 @@
+import CreditType.*
+
+
+interface ICardCasBack{
+    fun getCashBack(): Float
+}
+
+
+enum class CreditType(val color:String,val maxLimit:Int= 1000000) :ICardCasBack{                 //enum class inherit can interface
+    //enum constants
+    SILVER("gray", 5000) {               //ordinal 0      //name "SILVER"
+        override fun getCashBack(): Float = 0.02f
+    },
+    GOLD("gold") {                              //ordinal 1      //name  "GOLD"
+        override fun getCashBack(): Float = 0.04f
+    },
+    PLATINUM("black") {                        //ordinal 2      //name  "PLATINUM"
+        override fun getCashBack(): Float =0.06f
+    }
+}
+fun main(){
+    println(CreditType.PLATINUM.color)               //black
+    println(CreditType.GOLD.getCashBack())           //0.04
+}
+
+
