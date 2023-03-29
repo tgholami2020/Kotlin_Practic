2e4fb5fd34e752e8397d7a73df97efbcc6daa317
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-03-29 02:50:11 -0700

    Add files via upload

---
+++ ScopeFunWork#run().kt
@@ -0,0 +1,16 @@
+// run() work  {run is combination of let and with ,the return value is "lambda result",refer to context object by using" this"}
+// if you want to operate on a Nullable object and avoid NullPointerException use run().
+class Animal2(){
+    var name:String= "cat"
+    var age: Int= 3
+}
+fun main(){
+    val animal: Animal2 ? = null
+
+    val newAge=animal?.run {             //call ?. safe call, means if the animal is null not to print the properties
+        println(this.name)
+        println(this.age)
+        age+5
+    }
+    println("newAge:$newAge")
+}
