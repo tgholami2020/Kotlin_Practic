f53a839caffefa63ea0d066d4e38d84e5e32fcbd
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-04-02 05:59:31 -0700

    Add files via upload

--- GenericInFunction.kt
+++ GenericInFunction.kt
@@ -1,7 +1,8 @@
-
+//by defining generic we can use different data types later
 fun<T,E> JustForTest(param1:T,param2:E){
     println("$param1 and $param2")
 }
 fun main(){
+    // simply adding int and string data type here
     JustForTest<String,Int>("Hello", 20)
 }
