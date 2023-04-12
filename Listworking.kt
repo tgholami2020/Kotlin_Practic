db29fb1feaaddc70e4826e67c422045755244830
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-04-12 08:03:28 -0700

    Add files via upload

--- Listworking.kt
+++ Listworking.kt
@@ -1,20 +1,21 @@
 class ListWorking {
-    var nameOfStudents = mutableListOf("Ali","Ahmad","Mahmod","zahra")
-    var firstName= nameOfStudents.first()
+    var nameOfStudents = mutableListOf("Ali","Ahmad","Mahmod","zahra")      //mutable list of students
+    var firstName= nameOfStudents.first()                                   //fist name store here
 
 }
 
 
 fun main(){
+    //make object of class
 val listwork= ListWorking()
-    listwork.nameOfStudents
+    //printing list of students+
     println("names are : ${listwork.nameOfStudents}")
     println("first name is : ${listwork.firstName}")
     var nameOfStudents = mutableListOf("Ali","Ahmad","Mahmod","zahra")
-    nameOfStudents.add("Tahira")
+    nameOfStudents.add("Tahira")                                            //add one student to the list
     println(nameOfStudents)
-    nameOfStudents.remove("Ali")
-    nameOfStudents.removeAt(2)
+    nameOfStudents.remove("Ali")                                    //remove first student from list
+    nameOfStudents.removeAt(2)                                        //remove index 2 from list
     println(nameOfStudents)
 
 }
