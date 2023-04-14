b5b94662fa6489536a378c2bbaaba25a3295353f
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-04-14 05:31:00 -0700

    Add files via upload

---
+++ Collection(first,last,count).kt
@@ -0,0 +1,14 @@
+
+        fun main(){
+            val numbers= listOf("1","-2","3","-4","5","-6")
+            val first= numbers.first()                        //print 1
+            val last= numbers.last()                          //print -6
+            val totalCount= numbers.count()                   //print 6
+
+
+
+    println("the first number in the list is: $first")
+    println("the last number in the list is: $last")
+    println("total count of list is: $totalCount")
+}
+
