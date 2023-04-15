4035b12b4dc73f8c1403e92e5a5ac845661abe03
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-04-15 13:15:07 -0700

    Add files via upload

---
+++ AssociateBy,GroupBy.kt
@@ -0,0 +1,22 @@
+
+
+fun main(){
+    data class Person(val name: String, val city: String, val phone: String) // 1
+
+    val people = listOf(                                                     // 2
+        Person("John", "Boston", "+1-888-123456"),
+        Person("Sarah", "Munich", "+49-777-789123"),
+        Person("Svyatoslav", "Saint-Petersburg", "+7-999-456789"),
+        Person("Vasilisa", "Saint-Petersburg", "+7-999-123456"))
+
+    val phoneBook = people.associateBy { it.phone }                          // 3
+    val cityBook = people.associateBy(Person::phone, Person::city)           // 4
+    val peopleCities = people.groupBy(Person::city, Person::name)            // 5
+    val lastPersonCity = people.associateBy(Person::city, Person::name)      // 6
+
+    println("phone book is: $phoneBook")
+    println("City book is: $cityBook")
+    println("City of poeple are: $peopleCities")
+    println("last person city is: $lastPersonCity")
+
+}
