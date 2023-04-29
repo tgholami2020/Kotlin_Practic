d1f58118838791f81e7dddcc564e786ac07ffc9d
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-04-29 10:13:28 -0700

    Add files via upload

--- WhenStatement.kt
+++ WhenStatement.kt
@@ -3,11 +3,11 @@ fun main() {
 
     var number = 14
 
-    val whatTime= when (number) {
-        in 0..7 -> "good morning"
-        in 7..10 -> "go waking"
-        12 -> "time for lunch"
-        13,14 -> "go for nap"                            // use , here means or
+    val whatTime= when{
+        number in 0..7 -> "good morning"
+        number in 7..10 -> "time for waking"
+        number==12 -> "time for lunch"
+        number==1 || number==14 -> "time for nap"                            // use "," here means or
         else -> "good day"
     }
     println(whatTime)
