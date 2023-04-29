d79e8b28cf52cdbbacbead80bc7e44e8f41f4516
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-04-29 10:14:35 -0700

    Add files via upload

--- WhenStatement.kt
+++ WhenStatement.kt
@@ -7,7 +7,7 @@ fun main() {
         number in 0..7 -> "good morning"
         number in 7..10 -> "time for waking"
         number==12 -> "time for lunch"
-        number==1 || number==14 -> "time for nap"                            // use "," here means or
+        number==1 || number==14 -> "time for nap"                            
         else -> "good day"
     }
     println(whatTime)
