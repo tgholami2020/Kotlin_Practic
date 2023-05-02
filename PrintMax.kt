082246e53bbb66277f1c017b2fb4b32870f2bb47
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-05-02 13:27:46 -0700

    Add files via upload

--- PrintMax.kt
+++ PrintMax.kt
@@ -3,12 +3,13 @@ fun main(){
     val max= PrintMax(5,5)
 
 }
-fun PrintMax(num1: Int,num2:Int){
+fun PrintMax(num1: Int,num2:Int) {
     var maxNum=when{
         num1> num2 -> "num1 is bigger"
         num2> num1 -> "num2 is bigger"
         num1== num2-> "they are equal"
         else -> "Invalid"
+
     }
     println(maxNum)
 }
