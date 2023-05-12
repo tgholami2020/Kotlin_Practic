6b53787299c86b414bdd1dc86069525ce90b7002
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-05-12 02:49:20 -0700

    Add files via upload

--- HigherOrderFunction.kt
+++ HigherOrderFunction.kt
@@ -1,17 +1,18 @@
-
+//here is a higher order function that takes two int and one function as a parameter
 fun calculate (x:Int, y:Int,operation:(Int,Int)->Int):Int{
-return operation(x,y)
+return operation(x,y)                              //the higher order function return the result of operation
 }
 
-fun sum(x:Int, y:Int)= x+y
+fun sum(x:Int, y:Int)= x+y                         //declare a function that matches the operation signature
 
 fun main(){
 
-    val sumResult= calculate(3,6, ::sum)
+    val sumResult= calculate(3,6, ::sum)     //invoke the higher order function with two integer values and the sum function :: is th notation that reference  function by th name in kotlin
 
-    val multipleResult= calculate(3,6,{a,b -> a*b})
+    val multipleResult= calculate(3,6,{a,b -> a*b})  //invoke the higher order function passing in a lambda as a function argument
 
-    val reduce= calculate( 10,7,{a,b-> a-b})
+    val reduce= calculate( 10,7,{a,b-> a-b})         //invoke the higher order function passing in a lambda as a function argument
+
     println("sum Result is: $sumResult multiple result is: $multipleResult reduce result is: $reduce")
 
 }
