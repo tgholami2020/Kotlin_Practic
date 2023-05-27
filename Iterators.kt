f43bebe413fbdea287366f07c3a25aed3eafa286
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-05-27 14:31:57 -0700

    Add files via upload

---
+++ Iterators.kt
@@ -0,0 +1,39 @@
+
+fun main(){
+    //mySum
+val result= mySum()
+    println(result)
+
+    //secondIterator
+    val whileIterator= secondIteratorUsingWhile()
+    println(whileIterator)
+
+    //doWhile
+    val doWhile= doWhile()
+    println(doWhile)
+}
+
+fun mySum () {
+    var collectNumber = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    for (i in collectNumber) if (i % 2 == 0) {
+        println("$i")
+    }
+}
+
+fun secondIteratorUsingWhile(){
+    var num=1
+    while (num<7){
+        println(num)
+        num++
+    }
+}
+
+fun doWhile(){
+    var num =5
+    var sum=0
+    do {
+        sum+=num
+        num--
+    }while (num>0)
+    println("sum of numbers is $sum")
+}
