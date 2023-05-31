da7a16ecd232c1a334f3f25bbb496f198a41625b
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-05-31 04:05:43 -0700

    Add files via upload

---
+++ DelegationEx.kt
@@ -0,0 +1,17 @@
+
+class LazySample {
+    init {
+        println("created!")          
+    }
+
+    val lazyStr: String by lazy {
+        println("computed!")
+        "my lazy"
+    }
+}
+
+fun main() {
+    val sample = LazySample()
+    println("lazyStr = ${sample.lazyStr}")
+    println(" = ${sample.lazyStr}")
+}
