ed7cb65d7591b146a622c58271b89272d65dab24
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-06-12 17:44:43 -0700

    Add files via upload

--- CombiningTwoStrings.kt
+++ CombiningTwoStrings.kt
@@ -2,7 +2,11 @@
 fun main(){
     val name= "ali"
     val lastName="Gholami"
+    val combine= StringBuilder()
+    combine.append(name).append(lastName)
+    val completeName = combine.toString()
     println("$name $lastName")             //1
     println( name +  lastName)             //2
-
+    println(completeName)                  //3
+    
 }
