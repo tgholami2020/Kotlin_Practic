53c293447d429c055287d4a045b6b4bfa1f53465
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-06-14 16:51:40 -0700

    Add files via upload

---
+++ StringTemplate.kt
@@ -0,0 +1,8 @@
+
+
+fun main(){
+    var greeting = "kotlin"
+    println( "hello $greeting")
+    println("hello ${greeting.uppercase()}")
+    println("hello ${greeting.add("is","my favorite")}")
+}
