8d93bbab51299b058b652b28d87e2ff724cbd820
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-06-22 16:54:03 -0700

    Add files via upload

---
+++ Customer.kt
@@ -0,0 +1,9 @@
+
+
+class Customer (val id: Int,var email:String) {
+}
+fun main(){
+    val customer= Customer(2,"tahere.gholami67@gmail.com")
+    println(customer.id)
+    println(customer.email)
+}
