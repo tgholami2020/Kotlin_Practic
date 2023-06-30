7b89aa987f8582484e905f27111506c21fb337a9
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-06-29 17:57:55 -0700

    Add files via upload

--- ScopeFunctionPractice.kt
+++ ScopeFunctionPractice.kt
@@ -1,11 +1,18 @@
 
 
 fun main(){
- val number: Int?= 123
+ var number: Int = 2
     val x= number?.let {
         val number2 = it +1
         number2
     } ?: 3
-
+    fun getSquaredI()=(number*number).also {
+        number++
+    }
+    number.run {
+        number =3
+    }
+    println(number)
     println(x)
+    println(getSquaredI())
 }
