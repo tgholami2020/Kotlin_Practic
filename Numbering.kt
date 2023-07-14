209af258aa79c1f163443f24c66f025a080238e0
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-13 17:29:37 -0700

    Add files via upload

---
+++ Numbering.kt
@@ -0,0 +1,12 @@
+
+
+fun main(){
+
+    val day= 24
+    val week= 24*7
+    val month= week*4
+
+    println("one day has $day hours")
+    println("one week has $week hours")
+    println("one month has $month hours")
+}
