fd8a0604d933bf75c4ea972106a811260af8ed22
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-25 14:53:52 -0700

    Add files via upload

---
+++ typeAndOperarion.kt
@@ -0,0 +1,8 @@
+
+fun main(){
+    val age1:Double= 42.0           //control+ shift+p show Int
+    val age2:Double= 21.0
+
+    val avg1= (age1+age2)/2         // in here the result is 31.5 it is double .change type of age1 and age2 to Double
+    println(avg1)
+}
