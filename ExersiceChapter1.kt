8085e711ffb04a08a9672fb04adde93125b10e55
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-25 14:51:19 -0700

    Add files via upload

---
+++ ExersiceChapter1.kt
@@ -0,0 +1,19 @@
+
+fun main(){
+    val myAge: Int= 35
+    var averageAge :Int= (myAge+30)/2
+    println(averageAge)
+
+    val testNumber: Int= 48                     //when I put odd numbers the result is 1, otherwise 0.
+    val evenOdd= testNumber%2
+    println(evenOdd)
+
+    var answer= 0
+    answer +=10 * 10 shr(3)
+    println(answer)
+
+
+    var test= (5*3) -4 /(2*2)
+    println(test)
+
+}
