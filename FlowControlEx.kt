dcf35ec16119bb0d941bc25a5cbefbc120f3d045
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-27 17:24:18 -0700

    Add files via upload

--- FlowControlEx.kt
+++ FlowControlEx.kt
@@ -2,12 +2,18 @@
 fun main(){
     val myAge= 34
     val isTeenager=  myAge<19 && myAge>13                   //is the age is between 13 and 19?
-
+    println("$isTeenager")                                  //print false
+    
     val theirAge= 18
     val bothTeenager= theirAge<19 && theirAge>13
 
-    println("$isTeenager")
-    println("$bothTeenager")
+    println("$bothTeenager")                                //print true
+
+    val reader="Tahira"
+    val author="Richard Lucas"
+    val authorIsReader= reader== author
+
+    println(authorIsReader)                                //print false
 
 
 
