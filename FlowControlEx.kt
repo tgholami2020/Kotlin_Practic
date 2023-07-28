fd65301699956045131ade8f4ad420775374fc38
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-27 17:35:25 -0700

    Add files via upload

--- FlowControlEx.kt
+++ FlowControlEx.kt
@@ -3,7 +3,7 @@ fun main(){
     val myAge= 34
     val isTeenager=  myAge<19 && myAge>13                   //is the age is between 13 and 19?
     println("$isTeenager")                                  //print false
-    
+
     val theirAge= 18
     val bothTeenager= theirAge<19 && theirAge>13
 
@@ -14,6 +14,9 @@ fun main(){
     val authorIsReader= reader== author
 
     println(authorIsReader)                                //print false
+
+    val readerBeforeAuthor= reader > author                // if reader comes before author alphabetically
+    println(readerBeforeAuthor)                            //print true
 
 
 
