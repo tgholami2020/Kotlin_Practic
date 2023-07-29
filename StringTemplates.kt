d89b02ad2400dc9b9c3738af915b07e04a1c2125
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-29 15:37:39 -0700

    Add files via upload

---
+++ StringTemplates.kt
@@ -0,0 +1,9 @@
+
+fun main(){
+    var discountPercentage: Int=0
+    var offer:String=""
+    val item="Google Chromecast"
+    discountPercentage= 20
+    offer="Sale - up to $discountPercentage% discount on $item! Hurry up!"
+    println(offer)
+}
