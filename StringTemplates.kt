fe8388ade023722125749ba6f0669402ba500103
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-29 15:47:59 -0700

    Add files via upload

--- StringTemplates.kt
+++ StringTemplates.kt
@@ -6,4 +6,17 @@ fun main(){
     discountPercentage= 20
     offer="Sale - up to $discountPercentage% discount on $item! Hurry up!"
     println(offer)
+
+    //String concatenate
+
+    val numberOfAdults=20
+    val numberOfKids=30
+    val total= numberOfAdults+numberOfKids
+    println("the total party size is :$total")
+
+    //Message Formatting
+    val baseSalary= 5000
+    val bounceAmount=1000
+    val totalSalary=baseSalary+bounceAmount
+    println("congratulations for your bounce! you will receive a total of $totalSalary (additional bounce).")
 }
