70589b236dcae7905a546f562019dfc21a2deb96
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-07-29 16:11:40 -0700

    Add files via upload

--- StringTemplates.kt
+++ StringTemplates.kt
@@ -19,4 +19,33 @@ fun main(){
     val bounceAmount=1000
     val totalSalary=baseSalary+bounceAmount
     println("congratulations for your bounce! you will receive a total of $totalSalary (additional bounce).")
+
+    //Basic Math Operation
+    val firstNumber= 10
+    val secondNumber= 5
+    val result1= firstNumber+ secondNumber
+
+    println("$firstNumber + $secondNumber = $result1")
+
+    //Better way is writing add function for reusable code
+    fun add( a: Int,b: Int): Int{
+        val sum= a+b
+        return sum
+    }
+    fun subtract(a:Int,b:Int):Int{
+        val sub= a-b
+        return sub
+    }
+    val number1= 10
+    val number2= 5
+    val number3= 8
+    val result2= add(number1,number2)
+    val result3= add(number1,number3)
+    println("$number1 + $number2 = $result2")
+    println("$number1 + $number3 = $result3")
+    
+    val sub2=subtract(number1,number2)
+    println("$number1 - $number2 = $sub2")
+
+
 }
