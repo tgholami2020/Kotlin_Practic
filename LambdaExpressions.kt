561f8e7b77f29b1534e7683529ea12b65b3b0341
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-08-03 06:24:00 -0700

    Add files via upload

--- LambdaExpressions.kt
+++ LambdaExpressions.kt
@@ -1,10 +1,34 @@
 
 fun main(){
-    //how to store a function in variable
-    val trickFunction=trick
-    trick()
+    val coins: (Int)-> String={ it->
+        "$it quarters"
+    }
+    val cupCake:(Int)-> String={it->
+        "Have a cupcake"
+    }
+    val treatFunction= trickOrTreat(false, { "$it quarters" })               //remove coins name and write lambda expression instead
+    val trickFunction= trickOrTreat(true,null)
+    //repeat() is a higher order function that work like a loop
+    repeat(4){
+        treatFunction()
+    }
     trickFunction()
+}
+// a function can return another function/pass function as an argument
+fun trickOrTreat(isTrick: Boolean,extraTreat:((Int)-> String)?):() -> Unit{
+    if(isTrick){
+        return trick
+    }else{
+        if (extraTreat!=null){
+            println(extraTreat(5))
+        }
+        return treat
+    }
 }
 val trick={
     println("No treads!")
+}
+//use functions as data type
+val treat:() ->Unit ={
+    println("Have a treat!")
 }
