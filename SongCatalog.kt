5363680a2ffc98b47204b07cb94c54155fb1c21f
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-08-04 06:06:08 -0700

    Add files via upload

---
+++ SongCatalog.kt
@@ -0,0 +1,19 @@
+
+fun main(){
+    val brunoSong= Song("we dont talk about Burono","Enconto Cast",2022,1_000_000)
+    brunoSong.printDescription()
+    println(brunoSong.isPopular)
+}
+class Song(
+    val title:String,
+    val artist:String,
+    val yearPublished: Int,
+    val playCount:Int
+){
+    val isPopular:Boolean
+        get()= playCount>=1000
+
+    fun printDescription(){
+        println("$title,performed by $artist was released in $yearPublished.")
+    }
+}
