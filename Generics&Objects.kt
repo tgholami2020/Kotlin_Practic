f8cbdb7bd72bb19f8bab0bd26bf25dc1d469450c
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-08-13 16:56:13 -0700

    Add files via upload

--- Generics&Objects.kt
+++ Generics&Objects.kt
@@ -1,11 +1,16 @@
 
 fun main(){
+    println("${Quiz.answered} of ${Quiz.total} answered")
+}
+class Quiz{
     val question1= Question<String>("What is the ambivalent means?","having mix feeling",Difficulty.EASY)
     val question2= Question<Boolean>("Is the sky green?",false,Difficulty.MEDIUM)
     val question3= Question<Int>("Count the numbers of march month?",3,Difficulty.HARD)
-    println(question1.toString())
-    println(question2)
-    println(question3)
+
+    companion object StudentProgress{
+        var total : Int= 10
+        var answered :Int=3
+    }
 }
 data class Question<T>(
     val questionText:String,
@@ -18,3 +23,4 @@ enum class Difficulty{
     MEDIUM,
     HARD
 }
+
