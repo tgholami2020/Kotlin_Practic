32eac79aa4c748cdcf28683193d0c5dc05a035ed
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-08-16 14:34:59 -0700

    Add files via upload

--- Higher-OrderFunctionsWithcollections.kt
+++ Higher-OrderFunctionsWithcollections.kt
@@ -22,6 +22,28 @@ fun main(){
          softBakedMenu.forEach{
          println( "${it.name} : $${it.price}")
     }
+        //Using sortedBy
+        val alphabetically= cookies.sortedBy {
+            it.name
+        }
+        println("Alphabetically Sorted List: ")
+         alphabetically.forEach{
+        println(it.name )
+    }
+        //Using groupBy
+        val groupedMenu= cookies.groupBy {
+            it.softBaked
+        }
+        val softBakedMenu1= groupedMenu[true] ?: listOf()
+        val crunchyMenu = groupedMenu[false] ?: listOf()
+    println("Soft cookies:")
+    softBakedMenu1.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
     }
 class Cookie(
     val name: String,
