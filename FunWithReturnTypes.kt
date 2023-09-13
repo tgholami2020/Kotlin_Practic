cfc45a954cc8adb7925782b65848bae35a20f2b5
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-09-13 11:35:04 -0700

    Add files via upload

--- FunWithReturnTypes.kt
+++ FunWithReturnTypes.kt
@@ -5,9 +5,11 @@ fun main(){
     println("This cat is old ($catAge)")
     else
         println("This cat is young ($catAge)")
+
+    println(changeFunToLambda(3))
 }
 fun calculateCatAge(age: Int): Int = age*3   //There is more than one way to write function return in Kotlin
-val changeFunToLambda: (age:Int) ->Int = {a -> a*3}
+val changeFunToLambda: (Int) ->Int = {age -> age*3}
 // write another function to return a boolean
 fun checkAge(catAge: Int): Boolean{
     return catAge>14
