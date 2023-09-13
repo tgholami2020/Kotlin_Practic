95d81c1b62605ee4499146fc9bcadbe2edbac5d6
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-09-13 10:42:14 -0700

    Add files via upload

---
+++ AddParametersToFun.kt
@@ -0,0 +1,16 @@
+
+fun main(){
+    //you can name the arguments ot update the values
+calculate(first = 0,second= 200, massage = "is multiple of", multipleOf =20)
+}
+fun calculate(
+    first: Int=0,                           //add parameters to function
+    second: Int= 200,
+    massage:String,
+    multipleOf: Int){
+    for (i in first.. second){
+        if(i % multipleOf ==0){
+            println("$i $massage $multipleOf")
+        }
+    }
+}
