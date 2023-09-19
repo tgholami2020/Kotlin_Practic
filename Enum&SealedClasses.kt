bb1cac95a9e4ce0be891e222f6d823d7488a79fd
tgholami2020 <51695507+tgholami2020@users.noreply.github.com>
2023-09-19 09:38:02 -0700

    Add files via upload

--- Enum&SealedClasses.kt
+++ Enum&SealedClasses.kt
@@ -1,50 +1,85 @@
 
+import java.io.IOException
+import kotlin.Error
+
 fun main (){
     Repository.startFetch()
-    getResutl(Repository.getCurrentState())
+    getResult(Repository.getCurrentState())
 
     Repository.finishFetch()
-    getResutl(Repository.getCurrentState())
+    getResult(Repository.getCurrentState())
 
     Repository.error()
-    getResutl(Repository.getCurrentState())
+    getResult(Repository.getCurrentState())
+
+    Repository.anotherCustomFailure()
+    getResult(Repository.getCurrentState())
+
+    Repository.customFailure()
+    getResult(Repository.getCurrentState())
 
 }
-fun getResutl(result: Result){
+fun getResult(result: Result){
     return when(result){
-        Result.SUCCESS -> println("Success!")
-        Result.FAILURE -> println("Failure!")
-        Result.ERROR -> println("Error!")
-        Result.IDLE -> println("Idle")
-        Result.LOADING -> println("Loading...")
+        is Error -> {
+            println(result.message.toString())
+        }
+        is Success -> {
+            println(result.dataFetched ?: "Ensure you start the fetching")
+        }
+        is Loading -> {
+            println("Loading...")
+        }
+        is NotLoading -> {
+            println("Idle ")
+        }
+        is Failure.AnotherFailureException ->{
+            println(result.anotherFailureException.toString())
+        }
+        is Failure.CustomFailure ->{
+            println(result.customFailure.toString())
+        }
+        else -> {}
     }
 }
 //make an object class tells compiler that is a singleton and one instance is allowed
 object Repository{
 
-private var loadState: Result =Result.IDLE
+private var loadState: Result =  NotLoading
 private var dataFetched : String?= null
 
     fun startFetch(){
-        loadState = Result.LOADING
+        loadState = Loading
         dataFetched ="data"
     }
     fun finishFetch(){
-        loadState = Result.SUCCESS
+        loadState = Success(dataFetched)
         dataFetched = null
     }
     fun error(){
-        loadState = Result.ERROR
+        loadState = Error(exception= Exception("Exception"))
     }
     fun getCurrentState(): Result{
         return loadState
     }
+    fun anotherCustomFailure(){
+        loadState=Failure.AnotherFailureException(anotherFailureException = java.lang.NullPointerException("some this went wrong"))
+    }
+    fun customFailure(){
+        loadState= Failure.CustomFailure(customFailure = IOException("custom failure"))
+    }
 }
 
-enum class Result{
-    SUCCESS,
-    FAILURE,
-    ERROR,
-    IDLE,
-    LOADING
+sealed class Result 
+    data class Success(val dataFetched: String?) : Result()
+    data class Error(val exception: Exception) : Result()
+    object NotLoading : Result()
+    object Loading : Result()
+
+
+
+sealed class Failure :Result(){
+    data class CustomFailure(val customFailure: IOException): Failure()
+    data class AnotherFailureException(val anotherFailureException: NullPointerException):Failure()
+
 }
